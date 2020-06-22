6e66c8f3efd5e1513357fef509a32cf12a52bb9a
Erik <ecowley@protonmail.com>
2020-06-22 13:31:33 -0400

    start work on app

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+import dash
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+
+external_stylesheets = [
+    dbc.themes.BOOTSTRAP,
+    'https://use.fontawesome.com/releases/v5.9.0/css/all.css',
+]
